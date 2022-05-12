9b930dfda68ee30815305ca0755c48ec9ee391de
Nathan Silva Rodrigues <nathan.silva.rodrigues@gmail.com>
2022-05-12 08:21:48 -0300

    plots with different freq. values for tr_wave

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tokenize import String
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -14,60 +15,58 @@ def sin_wave(x, freq, y_sin, i):
     omega = 2.0 * np.pi * freq
     y_sin[i] = np.sin(omega * x)
 
-
-def tr_wave(x, y_tr, i):
-    omega = 2.0 * np.pi * 200
+#função que gera a onda triangular
+def tr_wave(x, y_tr, i, freq):
+    omega = 2.0 * np.pi * freq
     y_tr[i] = (2.75 / np.pi) * ((np.sin(omega * x)) - ((1 / 9) * np.sin(3 * omega * x)) + ((1 / 25) * np.sin(5 * omega * x))) 
 
-
-
+#função que compara as ondas
 def compare_waves(y_sin, y_tr, i, y_mod):
     if(y_sin[i] > y_tr[i]):
         y_mod[i] = 1
     else:
         y_mod[i] = -1
 
-
+#inicialização de variáveis
 y_seno = np.zeros(10000)
 y_tr = np.zeros(10000)
 y_mod = np.zeros(10000)
+
 #Preenchendo o eixo X
 eixo_x = np.linspace(0.0, 1.0, 10000)
 
+#Vetor de frequencias da onda triangular
+freqs_tr = [10.0, 50.0, 100.0, 200.0]
 
-#Recebendo frequência via input
+
+#Recebendo frequência da senóide via input
 freq_str = input("Valor da Frequencia em Hz: ")
 freq = float(freq_str)
 
-#Preenchendo o eixo Y 
-for i in range(10000):
-    sin_wave(eixo_x[i], freq, y_seno, i)
-    tr_wave(eixo_x[i], y_tr, i)
+for j in range(4):
+    #Preenchendo o eixo Y 
+    for i in range(10000):
+        sin_wave(eixo_x[i], freq, y_seno, i)
+        tr_wave(eixo_x[i], y_tr, i, freqs_tr[j])
 
+    for i in range(10000):
+        compare_waves(y_seno, y_tr, i, y_mod)
 
-for i in range(10000):
-    compare_waves(y_seno, y_tr, i, y_mod)
+    fig, axs = plt.subplots(2, figsize=(20,8))
+    axs[0].set(
+        ylabel = "Amplitude",
+        title = f"Senóide x Onda Triangular f = {freqs_tr[j]} Hz"
+    )
+    axs[0].plot(eixo_x, y_seno)
+    axs[0].plot(eixo_x, y_tr)
+    axs[0].legend(["Senóide", "Onda Triangular"])
 
-
-fig, axs = plt.subplots(2, figsize=(20,8))
-
-axs[0].set(
-    ylabel = "Amplitude",
-    title = "Senóide x Onda Triangular"
-)
-axs[0].plot(eixo_x, y_seno)
-axs[0].plot(eixo_x, y_tr)
-axs[0].legend(["Senóide", "Onda Triangular"])
-
-axs[1].set(
-    ylabel = "Amplitude",
-    xlabel = "Tempo (s)",
-    title = "Onda Modulada"
-)
-axs[1].plot(eixo_x, y_mod, 'tab:green')
-axs[1].legend(["Saída Modulada"])
-
-
-
-plt.savefig('output.png')
-plt.show()
+    axs[1].set(
+        ylabel = "Amplitude",
+        xlabel = "Tempo (s)",
+        title = "Onda Modulada"
+    )
+    axs[1].plot(eixo_x, y_mod, 'tab:green')
+    axs[1].legend(["Saída Modulada"])
+    plt.savefig(f'plot-f-{freqs_tr[j]}Hz.png')
+    plt.show()
